e006de0af962b3c1ff43917cf342b5a14804cd21
Sonya Karafeizova <sonqkarafeizova@gmail.com>
2023-06-25 13:57:55 +0300

    Add tasks for Lab-Associative Arrays
    Problem solving for Lab-Associative Arrays

---
+++ AssociativeArrays_Lab/OddOccurrences_03.java
@@ -0,0 +1,24 @@
+package AssociativeArrays_Lab;
+
+import java.util.*;
+
+public class OddOccurrences_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] inputArr = scanner.nextLine().split(" ");
+        LinkedHashMap<String, Integer> wordsMap = new LinkedHashMap<>();
+        for (String word : inputArr) {
+            word = word.toLowerCase();
+            wordsMap.putIfAbsent(word, 0);
+            wordsMap.put(word,wordsMap.get(word)+1);
+
+        }
+        List<String> result=new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordsMap.entrySet()) {
+            if(entry.getValue()%2!=0){
+                result.add(entry.getKey());
+            }
+        }
+        System.out.println(String.join(", ",result));
+    }
+}
---
+++ AssociativeArrays_Lab/WordFilter_04.java
@@ -0,0 +1,22 @@
+package AssociativeArrays_Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WordFilter_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] wordsArr = Arrays.stream(scanner.nextLine().split(" "))
+                .filter(w ->w.length()%2==0)
+                .toArray(String[]::new);
+        System.out.println(String.join(System.lineSeparator(),wordsArr));
+
+    }
+//static boolean evenWord(String s){
+//    return s.length()%2==0;
+        }
+
+
+
+
